6700ab3d6a4352f2fbd626665c1e0079a8e2a2ea
Aditya Aman <72183883+aditya-1967@users.noreply.github.com>
2022-08-18 16:08:31 +0530

    Add files via upload

---
+++ print_all_subsequence.py
@@ -0,0 +1,19 @@
+# Print all Subsequence
+
+arr = [3,1,2,4]
+
+res = []
+temp = []
+def func(index):
+    if index == len(arr):
+        print(temp)
+        return
+        # res.append(temp)
+        # return res
+    temp.append(arr[index])
+    func(index + 1)
+    temp.pop()
+    func(index + 1)
+
+func(0)
+# print(res)
